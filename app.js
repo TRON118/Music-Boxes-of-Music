0cfec87c39ef4923e115b35f8888cc5d8707e39a
TRON118 <lisa.makin@gmail.com>
2017-03-04 13:48:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var url = 'https://bcw.blob.core.windows.net/public/img/sounds/scale-00';
+var url = 'https://bcw.blob.core.windows.net/public/img/sounds/scale-0';
 var boombox = document.getElementById('boombox')
 
 function playTune(num){
-  
   var tuneUrl = url + num + '.mp3'
+  
+  if(num < 10){
+    tuneUrl += '0'
+  }
+  
+  tuneUrl += num + '.mp3'
+  
   console.log(tuneUrl)
   
   boombox.src=tuneUrl
